73557220990b8d76307e4e34a23c6dda96f83fe5
samson wong <steelerwong2005@hotmail.com>
2020-05-29 14:01:47 -0400

    index1 and script1 and css1 is the good one

--- script.js
+++ script.js
@@ -4,103 +4,129 @@ var timerDisplay = document.querySelector("#timer");
 var questionsDisplay = document.querySelector("#questions");
 var questionContentDisplay = document.querySelector("#questionContent");
 
-// Create a user with Name and final score
-// var user = {
-//     name: username.value.trim();
-//     finalscore: score;
-// }
-// localStorage.setItem('user', user);
 
 var q1 = {
-    question: "What does Gandlaf Full Time job ?",
+    question: "Gandlaf The Gray Full Time job ?",
     choice1:"1. Wizard",
     choice2:"2. Witch",
     choice3:"3. Gardner",
-    answer:1
+    answer:"1"
 }
 
 var q2 = {
-    question: "What does Samwise Gamgie Full Time job ?",
-    choice1: "1. Wizard",
-    choice2: "2. Witch",
+    question: " Samwise Gamgie Full Time job ?",
+    choice1: "1. Fighter",
+    choice2: "2. Waiting for Lucy",
     choice3: "3. Gardner",
-    answer:3
+    answer:"3"
+}
+
+var q3 = {
+  question: "Frodo's Full Time job ?",
+  choice1: "1. Book Worm",
+  choice2: "2. Destroy the ring",
+  choice3: "3. Clean house for Bilbo",
+  answer:"2"
 }
 
 var totalSeconds = 75;
 var secondsElapsed = 0;
 var interval;
-var questionList =[q1];
+var questionList =[q1,q2,q3];
+var questionLen = questionList.length;
+var livequestion;
 var score = 0;
+var tempwait;
+var goOn = true;
+var index = 0;
 
-
+// User hit Start quiz, so closing first page
 quizEl.addEventListener("click", closepage);
 
-
-
+// After page is closed, kick off quiz questions.
 function closepage(event) {
     console.log("Start questions page");
     firstpgEl.style.display = "none";
-    startquiz();
+    startquiz(0);
+     
 }
 
-function startquiz() {
-    startTimer();
-    questionsDisplay.style.display = "block";
 
-    //dynamically loop thru each questions, display
-    // store user ans
-    // if incorrect, update timer, show incorrect
-    
-    for (var i = 0; i < questionList.length; i++) {
+function startquiz(i) {
+  console.log("Starting i value is:", i);
+  startTimer();
+  
+  questionsDisplay.style.display = "block";
+  questionContentDisplay.innerHTML = "";
+  //questionContentDisplay = " ";
 
-        var livequestion = questionList[i];
-    
-        var ask = document.createElement("p");
-        var button1 = document.createElement("button");
-        var button2 = document.createElement("button");
-        var button3 = document.createElement("button");
-        var correct = document.createElement("p");
-        var wrong = document.createElement("p");
+  livequestion = "";
+  livequestion = questionList[i];
 
-        ask.textContent = livequestion.question;
-        button1.textContent = livequestion.choice1;
-        button1.setAttribute("data-index", 1);
-        button2.textContent = livequestion.choice2;
-        button2.setAttribute("data-index", 2);
-        button3.textContent = livequestion.choice3;
-        button3.setAttribute("data-index", 3);
-        correct.textContent = "Correct, you are Smart !";
-        wrong.textContent = "Sorry, you are WRONG ";
-        
-        questionContentDisplay.appendChild(ask);
-        questionContentDisplay.appendChild(button1);
-        questionContentDisplay.appendChild(button2);
-        questionContentDisplay.appendChild(button3);
-        console.log("Button 1 attribute: ", button1.getAttribute("data-index"));
-        console.log("Button 2 attribute: ", button2.getAttribute("data-index"));
-        console.log("Button 3 attribute: ", button3.getAttribute("data-index"));
+  var ask = document.createElement("p");
+  var button1 = document.createElement("button");
+  var button2 = document.createElement("button");
+  var button3 = document.createElement("button");
+  var correct = document.createElement("p");
+  var wrong = document.createElement("p");
 
-        questionContentDisplay.addEventListener("click", function (event) {
-            event.stopPropagation();
-            if (event.target.matches("button") == true) {
-                var userchoice = event.target.getAttribute("data-index");
-                console.log("User clicked on choice:", userchoice);
-                if (userchoice == livequestion.answer) {
-                    score++;
-                    questionContentDisplay.appendChild(correct);
-                    console.log("current score: ", score);
-                } else {
-                    secondsElapsed = secondsElapsed + 10;
-                    questionContentDisplay.appendChild(wrong);
-                    console.log("current score: ", score);
-                }
-            }
-        })
+  ask.textContent = livequestion.question;
+  console.log("The object we are dealing with is: ", questionList[i]);
+  button1.textContent = livequestion.choice1;
+  button1.setAttribute("data-index", 1);
+  button2.textContent = livequestion.choice2;
+  button2.setAttribute("data-index", 2);
+  button3.textContent = livequestion.choice3;
+  button3.setAttribute("data-index", 3);
+  correct.textContent = "Correct, you are Smart !";
+  wrong.textContent = "Sorry, you are WRONG ";
+  
+  questionContentDisplay.appendChild(ask);
+  questionContentDisplay.appendChild(button1);
+  questionContentDisplay.appendChild(button2);
+  questionContentDisplay.appendChild(button3);
+  console.log("Button 1 attribute: ", button1.getAttribute("data-index"));
+  console.log("Button 2 attribute: ", button2.getAttribute("data-index"));
+  console.log("Button 3 attribute: ", button3.getAttribute("data-index"));
+  console.log("Correct answer (livequestion) supposed to be: ", livequestion.answer);
 
+  //questionContentDisplay.addEventListener("click", function (event) {
+  questionContentDisplay.addEventListener("click", checkAnswer);
+  
+  
+  function checkAnswer (event) {
+    event.stopPropagation();
+    event.preventDefault();
+    console.log("A CLICK event occured - User clicked button");
 
+    if (event.target.matches("button") == true) {  
+      var userchoice = event.target.getAttribute("data-index");
+      console.log("User clicked on choice:", userchoice);
+      console.log("Correct Choice is:", livequestion.answer);
+      if (userchoice == livequestion.answer) {
+          score++;
+          questionContentDisplay.appendChild(correct);
+          alert("Right !");
+          console.log("You got it Right ! current score: ", score);
+      } else {
+          secondsElapsed = secondsElapsed + 10;
+          questionContentDisplay.appendChild(wrong);
+          alert("wrong !");
+          console.log("Wrong ans: current score: ", score);
       }
-}
+      console.log("Towards End of IF: Value of i before startquiz:", i);
+
+      // sleep(500);
+
+      if (questionLen > (i + 1)) {
+        startquiz(++i);
+      } else {
+        console.log("Final score: ", score);
+      }
+    }// End Outer If 
+  } //End Event Listener for click
+
+} // End Function StartTime
 
 
 // Utility Function
@@ -108,7 +134,7 @@ function startquiz() {
 // This function is where the "time" aspect of the timer runs
 // Notice no settings are changed other than to increment the secondsElapsed var
 function startTimer() {
-    totalSeconds = 35;
+    totalSeconds = 135;
     clearInterval(interval);
   
     // we only want to start the timer if minutes is > 0
---
+++ script1.js
@@ -0,0 +1,184 @@
+var firstpageEl = document.querySelector("#firstpage");
+var quizEl = document.querySelector("#quiz");
+var questionsDisplay = document.querySelector("#question-container");
+var questionEl = document.querySelector("#questions");
+var choicesDisplay = document.querySelector("#choices");
+var choiceAEl = document.querySelector("#choiceA");
+var choiceBEl = document.querySelector("#choiceB");
+var choiceCEl = document.querySelector("#choiceC");
+var resultEl = document.querySelector("#result");
+var timerDisplay = document.querySelector("#timer");
+
+// Define Variable
+var q1 = {
+    question: "Gandlaf The Gray Full Time job ?",
+    choice1:"1. Wizard",
+    choice2:"2. Witch",
+    choice3:"3. Gardner",
+    answer:"1"
+}
+
+var q2 = {
+    question: " Samwise Gamgie Full Time job ?",
+    choice1: "1. Fighter",
+    choice2: "2. Waiting for Lucy",
+    choice3: "3. Gardner",
+    answer:"3"
+}
+
+var q3 = {
+  question: "Frodo's Full Time job ?",
+  choice1: "1. Book Worm",
+  choice2: "2. Destroy the ring",
+  choice3: "3. Clean house for Bilbo",
+  answer:"2"
+}
+
+var totalSeconds = 75;
+var secondsElapsed = 0;
+var interval;
+var score = 0;
+var questionList =[q1,q2,q3];
+var questionLen = questionList.length - 1;
+var index = 0;
+var livequestion;
+var TIMER;
+
+
+// User hit Start quiz, so closing first page
+quizEl.addEventListener("click", startQuiz);
+
+// After page is closed, kick off quiz questions.
+function startQuiz(event) {
+    console.log("Start questions page");
+    firstpageEl.style.display = "none";
+    questionsDisplay.style.display = "block";
+    startTimer();
+    renderQuestion(); // display only
+}
+
+
+function renderQuestion() {
+  console.log("Start Render Question");
+  //choicesDisplay.innerHTML = "";
+  livequestion = questionList[index];
+  
+  console.log("The object we are dealing with is: ", questionList[index]);
+  
+  questionEl.textContent = livequestion.question;
+  choiceAEl.textContent = livequestion.choice1;
+  choiceBEl.textContent = livequestion.choice2;
+  choiceCEl.textContent = livequestion.choice3;
+  resultEl.textContent = "";
+  console.log("question is: ", questionList[index].question);
+  console.log("Choice 1 is : ", questionList[index].choice1);
+  console.log("Choice 2 is : ", questionList[index].choice2);
+  console.log("Choice 3 is : ", questionList[index].choice3);
+  console.log("Choice 1 attribute: ", choiceAEl.getAttribute("data-index"));
+  console.log("Choice 2 attribute: ", choiceBEl.getAttribute("data-index"));
+  console.log("Choice 3 attribute: ", choiceCEl.getAttribute("data-index"));
+  console.log("Correct answer (livequestion) supposed to be: ", livequestion.answer);
+}
+
+choicesDisplay.addEventListener("click", checkAnswer);
+
+function checkAnswer(event) {
+  event.stopPropagation();
+  console.log(" Enter Function checkAnswered");
+
+  //if (event.target.matches("button") == true) {
+      
+  let userchoice = event.target.getAttribute("data-index");
+  console.log("User clicked on choice:", userchoice);
+  console.log("Correct Choice is:", livequestion.answer);
+
+  if (userchoice == livequestion.answer) {
+      score++;
+      resultEl.textContent="YOU ARE CORRECT";
+      alert("Right !");
+      console.log("You got it Right ! current score: ", score);
+  } else {
+      secondsElapsed = secondsElapsed + 10;
+      resultEl.textContent="YOU ARE WRONG";
+      alert("wrong !");
+      console.log("Wrong ans: current score: ", score);
+  }
+
+  nextQuestions();
+} //End If
+
+function nextQuestions () {
+  console.log ("Index len is: ", index, "question len :", questionLen)
+  if (index < questionLen) {
+    index++;
+    renderQuestion();
+  }
+}
+
+
+// Utility Function
+
+
+// This function is where the "time" aspect of the timer runs
+// Notice no settings are changed other than to increment the secondsElapsed var
+function startTimer() {
+    totalSeconds = 135;
+    clearInterval(interval);
+  
+    // we only want to start the timer if minutes is > 0
+    if (totalSeconds > 0) {    
+      /* the "interval" variable here using "setInterval()" begins the recurring increment of the 
+         secondsElapsed variable which is used to check if the time is up */
+        interval = setInterval(function() {
+          secondsElapsed++;
+          //So renderTime() is called here once every second.
+          renderTime();
+        }, 1000);
+    } else {
+      alert("Minutes of work/rest must be greater than 0.")
+    }
+  }
+
+  /* This function stops the interval and also resets secondsElapsed 
+   and calls "setTime()" which effectively reset the timer 
+   to the input selections workMinutesInput.value and restMinutesInput.value */
+function stopTimer() {
+    secondsElapsed = 0;
+    clearInterval(interval);
+    renderTime();
+  }
+
+
+
+//This function does 2 things. displays the time and checks to see if time is up.
+function renderTime() {
+    // When renderTime is called it sets the textContent for the timer html...
+    // minutesDisplay.textContent = getFormattedMinutes();
+    timerDisplay.textContent = "Timer: " + getFormattedSeconds();
+  
+   // ..and then checks to see if the time has run out
+    if (secondsElapsed >= totalSeconds) {
+      alert("Times Up");
+      stopTimer();
+    }
+  }
+  
+function getFormattedSeconds() {
+    //var secondsLeft = (totalSeconds - secondsElapsed) % 60;
+    var secondsLeft = (totalSeconds - secondsElapsed)
+  
+    var formattedSeconds;
+  
+    if (secondsLeft < 10) {
+      formattedSeconds = "0" + secondsLeft;
+    } else {
+      formattedSeconds = secondsLeft;
+    }
+  
+    return formattedSeconds;
+  }
+
+
+
+
+
